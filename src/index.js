8ff0562f23f07af219ffc45344d8a2b815365d9f
Minh Nguyen <minh.nguyen@ad.thefirma.fi>
2018-06-13 15:53:45 +0300

    changes on index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+let playerNameSubmitted = false;
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -43,9 +43,65 @@ class Board extends React.Component {
   }
 }
 
+ /*TODO : Name form*/ 
+/*class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.players = 
+    {
+        player1: '',
+        player2: ''
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    if(this.players.player1.length == 0 ){
+        this.players.player1 = "player 1";
+    }
+   if(this.players.player2.length == 0){
+       this.players.player2 = "player 2";
+   }
+     playerNameSubmitted = true; 
+      
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div class="col-sm-6">
+        <label>
+            Player 1 (X):
+        </label>
+          <input name="p1box" type="text" onChange={this.handleInputChange} placeholder="player 1 name" />
+        <label>
+            Player 2 (O):
+        </label>
+        <input name="p2box" type="text" onChange={this.handleInputChange} placeholder="player 2 name" />
+        </div>
+      </form>
+    );
+  }
+}*/
+
+
 class Game extends React.Component {
   constructor(props) {
     super(props);
+      
+
     this.state = {
       history: [
         {
@@ -53,8 +109,11 @@ class Game extends React.Component {
         }
       ],
       stepNumber: 0,
-      xIsNext: true
+      xIsNext: true,
+        player1: '',
+        player2: '',
     };
+  this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleClick(i) {
@@ -82,12 +141,24 @@ class Game extends React.Component {
       xIsNext: (step % 2) === 0
     });
   }
+    
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+        [name]: value
+      
+    });
+
+  }
 
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
-
+      
     const moves = history.map((step, move) => {
       const desc = move ?
         'Go to move #' + move :
@@ -98,37 +169,45 @@ class Game extends React.Component {
         </li>
       );
     });
-
+    let p1 = (this.state.player1 == "") ? "player 1" : this.state.player1;
+    let p2 = (this.state.player2 == "") ? "player 2" : this.state.player2;
     let status;
     let victor;
 
     if (winner) {
         if(winner === "X"){
-            victor = "Bibek";
+            victor = p1;
         }
         else{
-            victor = "Minh";
+            victor = p2;
         } 
       status = "Winner: " + victor;
         
     } else {
-      status = "Next player: " + (this.state.xIsNext ? "Bibek" : "Minh");
+      status = "Next player: " + (this.state.xIsNext ? p1 : p2);
     }
-
     return (
       <div className="game" >
-        <p class="mb-3 h1"> WELCOME TO GAME OF TICTACTOE!!!</p>
+        {/*Player names region*/}
+        <div class="col-sm-5">
+            <label>Player 1 (X):</label>
+              <input name="player1" type="text" onChange={this.handleInputChange} placeholder="player 1 name" /> <br/>
+            <label>Player 2 (O):</label>
+            <input name="player2" type="text" onChange={this.handleInputChange} placeholder="player 2 name" />
+        </div>
+
+        {/*Board Region*/}
+        <p class="mb-3 h"> WELCOME TO GAME OF TICTACTOE!!!</p>
         <div className="game-board">
           <Board
             squares={current.squares}
             onClick={i => this.handleClick(i)}
           />
-          
+        
+        {/*Status Region*/}
         </div>
         <div className="game-info">
-        
           <div>{status}</div>
-
           <ol>{moves}</ol>
         </div>
       </div>
